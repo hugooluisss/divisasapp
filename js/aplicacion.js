c5ec04cd1428557bda297be3b472ad19f2e29e66
Hugo Luis Santiago Altamirano <hugooluisss@gmail.com>
2016-04-05 00:11:03 -0500

    Terminado, falta agregar los comerciales

--- js/aplicacion.js
+++ js/aplicacion.js
@@ -1,13 +1,237 @@
-//var server = 'http://192.168.0.111/justify/';
-var server = 'http://panel.justify.com.mx/';
-var key_google = "AIzaSyAFa9dJnYA73gbJSdoery99NTOzUgbdL3w";
-
-
-var goldStar = {
-	path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-	fillColor: 'yellow',
-	fillOpacity: 0.8,
-	scale: 1,
-	strokeColor: 'gold',
-	strokeWeight: 14
+var paises = {
+	"divisas": [
+		{"id": "AED", "val": "Dirham de los Emiratos Árabes Unidos"},
+		{"id": "AFN", "val": "Afgani afgano"},
+		{"id": "ALL", "val": "Lek albano"},
+		{"id": "AMD", "val": "Dram armenio"},
+		{"id": "ANG", "val": "Florín de las Antillas Holandesas"},
+		{"id": "AOA", "val": "Kwanza angoleño"},
+		{"id": "ARS", "val": "Peso argentino"},
+		{"id": "AUD", "val": "Dólar australiano"},
+		{"id": "AWG", "val": "Florín arubeño"},
+		{"id": "AZM", "val": "Manat azerbaiyano"},
+		{"id": "BAM", "val": "marco convertible de Bosnia-Herzegovina"},
+		{"id": "BBD", "val": "Dólar de Barbados"},
+		{"id": "BDT", "val": "Taka de Bangladesh"},
+		{"id": "BGN", "val": "Lev búlgaro"},
+		{"id": "BHD", "val": "Dinar bahreiní"},
+		{"id": "BIF", "val": "Franco burundés"},
+		{"id": "BMD", "val": "Dólar de Bermuda"},
+		{"id": "BND", "val": "Dólar de Brunei"},
+		{"id": "BOB", "val": "Boliviano"},
+		{"id": "BOV", "val": "Mvdol boliviano (código de fondos)"},
+		{"id": "BRL", "val": "Real brasileño"},
+		{"id": "BSD", "val": "Dólar bahameño"},
+		{"id": "BTN", "val": "Ngultrum de Bután"},
+		{"id": "BWP", "val": "Pula de Botswana"},
+		{"id": "BYR", "val": "Rublo bielorruso"},
+		{"id": "BZD", "val": "Dólar de Belice"},
+		{"id": "CAD", "val": "Dólar canadiense"},
+		{"id": "CDF", "val": "Franco congoleño"},
+		{"id": "CHF", "val": "Franco suizo"},
+		{"id": "CLF", "val": "Unidades de fomento chilenas (código de fondos)"},
+		{"id": "CLP", "val": "Peso chileno"},
+		{"id": "CNY", "val": "Yuan Renminbi de China"},
+		{"id": "COP", "val": "Peso colombiano"},
+		{"id": "COU", "val": "Unidad de valor real colombiana (añadida al COP)"},
+		{"id": "CRC", "val": "Colón costarricense"},
+		{"id": "CSD", "val": "Dinar serbio (It was replaced by RSD on October 25, 2006)"},
+		{"id": "CUP", "val": "Peso cubano"},
+		{"id": "CVE", "val": "Escudo caboverdiano"},
+		{"id": "CYP", "val": "Libra chipriota"},
+		{"id": "CZK", "val": "Koruna checo"},
+		{"id": "DJF", "val": "Franco yibutiano"},
+		{"id": "DKK", "val": "Corona danesa"},
+		{"id": "DOP", "val": "Peso dominicano"},
+		{"id": "DZD", "val": "Dinar algerino"},
+		{"id": "EEK", "val": "Corona estonia"},
+		{"id": "EGP", "val": "Libra egipcia"},
+		{"id": "ERN", "val": "Nakfa eritreo"},
+		{"id": "ETB", "val": "Birr etíope"},
+		{"id": "EUR", "val": "Euro"},
+		{"id": "FJD", "val": "Dólar fijiano"},
+		{"id": "FKP", "val": "Libra malvinense"},
+		{"id": "GBP", "val": "Libra esterlina (libra de Gran Bretaña)"},
+		{"id": "GEL", "val": "Lari georgiano"},
+		{"id": "GHC", "val": "Cedi ghanés"},
+		{"id": "GIP", "val": "Libra de Gibraltar"},
+		{"id": "GMD", "val": "Dalasi gambiano"},
+		{"id": "GNF", "val": "Franco guineano"},
+		{"id": "GTQ", "val": "Quetzal guatemalteco"},
+		{"id": "GYD", "val": "Dólar guyanés"},
+		{"id": "HKD", "val": "Dólar de Hong Kong"},
+		{"id": "HNL", "val": "Lempira hondureño"},
+		{"id": "HRK", "val": "Kuna croata"},
+		{"id": "HTG", "val": "Gourde haitiano"},
+		{"id": "HUF", "val": "Forint húngaro"},
+		{"id": "IDR", "val": "Rupiah indonesia"},
+		{"id": "ILS", "val": "Nuevo shequel israelí"},
+		{"id": "INR", "val": "Rupia india"},
+		{"id": "IQD", "val": "Dinar iraqí"},
+		{"id": "IRR", "val": "Rial iraní"},
+		{"id": "ISK", "val": "Króna islandesa"},
+		{"id": "JMD", "val": "Dólar jamaicano"},
+		{"id": "JOD", "val": "Dinar jordano"},
+		{"id": "JPY", "val": "Yen japonés"},
+		{"id": "KES", "val": "Chelín keniata"},
+		{"id": "KGS", "val": "Som kirguís (de Kirguistán)"},
+		{"id": "KHR", "val": "Riel camboyano"},
+		{"id": "KMF", "val": "Franco comoriano (de Comoras)"},
+		{"id": "KPW", "val": "Won norcoreano"},
+		{"id": "KRW", "val": "Won surcoreano"},
+		{"id": "KWD", "val": "Dinar kuwaití"},
+		{"id": "KYD", "val": "Dólar caimano (de Islas Caimán)"},
+		{"id": "KZT", "val": "Tenge kazajo (de Kazajstán)"},
+		{"id": "LAK", "val": "Kip lao"},
+		{"id": "LBP", "val": "Libra libanesa"},
+		{"id": "LKR", "val": "Rupia de Sri Lanka"},
+		{"id": "LRD", "val": "Dólar liberiano"},
+		{"id": "LSL", "val": "Loti lesothense (de Lesotho)"},
+		{"id": "LTL", "val": "Litas lituano"},
+		{"id": "LVL", "val": "Lat letón"},
+		{"id": "LYD", "val": "Dinar libio"},
+		{"id": "MAD", "val": "Dirham marroquí"},
+		{"id": "MDL", "val": "Leu moldavo"}, 
+		{"id": "MGA", "val": "Ariary malgache (de Madagascar)"},
+		{"id": "MKD", "val": "Denar macedonio"},
+		{"id": "MMK", "val": "Kyat myanmaro"},
+		{"id": "MNT", "val": "Tughrik mongol"},
+		{"id": "MOP", "val": "Pataca de Macao"},
+		{"id": "MRO", "val": "Ouguiya mauritana"},
+		{"id": "MTL", "val": "Lira maltesa"},
+		{"id": "MUR", "val": "Rupia mauricia"},
+		{"id": "MVR", "val": "Rufiyaa maldiva"},
+		{"id": "MWK", "val": "Kwacha malawiano"},
+		{"id": "MXN", "val": "Peso mexicano"},
+		{"id": "MXV", "val": "Unidad de Inversión (UDI) mexicana (código de fondos)"},
+		{"id": "MYR", "val": "Ringgit malayo"},
+		{"id": "MZM", "val": "Metical mozambiqueño"},
+		{"id": "NAD", "val": "Dólar namibio"},
+		{"id": "NGN", "val": "Naira nigeriana"},
+		{"id": "NIO", "val": "Córdoba nicaragüense"},
+		{"id": "NOK", "val": "Corona noruega"},
+		{"id": "NPR", "val": "Rupia nepalesa"},
+		{"id": "NZD", "val": "Dólar neozelandés"},
+		{"id": "OMR", "val": "Rial omaní (de Omán)"},
+		{"id": "PAB", "val": "Balboa panameña"},
+		{"id": "PEN", "val": "Nuevo sol peruano"},
+		{"id": "PGK", "val": "Kina de Papúa Nueva Guinea"},
+		{"id": "PHP", "val": "Peso filipino"},
+		{"id": "PKR", "val": "Rupia pakistaní"},
+		{"id": "PLN", "val": "zloty polaco"},
+		{"id": "PYG", "val": "Guaraní paraguayo"},
+		{"id": "QAR", "val": "Rial qatarí"},
+		{"id": "RON", "val": "Leu rumano (desde el 1 de julio de 2005)"},
+		{"id": "RUB", "val": "Rublo ruso"},
+		{"id": "RWF", "val": "Franco ruandés"},
+		{"id": "SAR", "val": "Riyal saudí"},
+		{"id": "SBD", "val": "Dólar de las Islas Salomón"},
+		{"id": "SCR", "val": "Rupia de Seychelles"},
+		{"id": "SDD", "val": "Dinar sudanés"},
+		{"id": "SEK", "val": "Corona sueca"},
+		{"id": "SGD", "val": "Dólar de Singapur"},
+		{"id": "SHP", "val": "Libra de Santa Helena"},
+		{"id": "SKK", "val": "Corona eslovaca"},
+		{"id": "SLL", "val": "Leone de Sierra Leona"},
+		{"id": "SOS", "val": "Chelín somalí"},
+		{"id": "SRD", "val": "Dólar surinamés (desde el 1 de enero de 2004)"},
+		{"id": "STD", "val": "Dobra de Santo Tomé y Príncipe"},
+		{"id": "SYP", "val": "Libra siria"},
+		{"id": "SZL", "val": "Lilangeni suazi (de Suazilandia)"},
+		{"id": "THB", "val": "Baht tailandés"},
+		{"id": "TJS", "val": "Somoni tayik (de Tayikistán)"},
+		{"id": "TMM", "val": "Manat turcomano"},
+		{"id": "TND", "val": "Dinar tunecino"},
+		{"id": "TOP", "val": "Pa’anga tongano"},
+		{"id": "TRY", "val": "Nueva lira turca"},
+		{"id": "TTD", "val": "Dólar de Trinidad y Tobago"},
+		{"id": "TWD", "val": "Dólar taiwanés"},
+		{"id": "TZS", "val": "Chelín tanzano"},
+		{"id": "UAH", "val": "Hryvnia ucraniana"},
+		{"id": "UGX", "val": "Chelín ugandés"},
+		{"id": "USD", "val": "Dólar estadounidense"},
+		{"id": "USN", "val": "Dólar estadounidense (Siguiente día) (código de fondos)"},
+		{"id": "USS", "val": "United States dollar (Mismo día) (código de fondos)"},
+		{"id": "UYU", "val": "Peso uruguayo"},
+		{"id": "UZS", "val": "Som uzbeco"},
+		{"id": "VEB", "val": "Bolívar venezolano"},
+		{"id": "VEF", "val": "Bolívar fuerte venezolano"},
+		{"id": "VND", "val": "Dong vietnamita"},
+		{"id": "VUV", "val": "Vatu vanuatense"},
+		{"id": "WST", "val": "Tala samoana"},
+		{"id": "XAF", "val": "Franco CFA"},
+		{"id": "XAG", "val": "Onza de plata"},
+		{"id": "XAU", "val": "Onza de oro"},
+		{"id": "XBA", "val": "European Composite Unit (EURCO) (Bonds market unit)"},
+		{"id": "XBB", "val": "European Monetary Unit (E.M.U.-6) (Bonds market unit)"},
+		{"id": "XBC", "val": "European Unit of Account 9 (E.U.A.-9) (Bonds market unit)"},
+		{"id": "XBD", "val": "European Unit of Account 17 (E.U.A.-17) (Bonds market unit)"},
+		{"id": "XCD", "val": "Dólar del Caribe Oriental"},
+		{"id": "XDR", "val": "Special Drawing Rights (FMI)"},
+		{"id": "XFO", "val": "Franco de oro (Special settlement currency)"},
+		{"id": "XFU", "val": "Franco UIC (Special settlement currency)"},
+		{"id": "XOF", "val": "Franco CFA"},
+		{"id": "XPD", "val": "Onza de paladio"},
+		{"id": "XPF", "val": "Franco CFP"},
+		{"id": "XPT", "val": "Onza de platino"},
+		{"id": "XTS", "val": "Reservado para pruebas"},
+		{"id": "XXX", "val": "Sin divisa"},
+		{"id": "YER", "val": "Rial yemení (de Yemen)"},
+		{"id": "ZAR", "val": "Rand sudafricano"},
+		{"id": "ZMK", "val": "Kwacha zambiano"},
+		{"id": "ZWD", "val": "Dólar zimbabuense"}
+	]
 };
+
+$( document ).ready(function(){
+	$("[data-mask]").inputmask();
+	
+	$.each(paises.divisas, function(i, item) {
+		$("#selOrigen").append($("<option />", {
+			"value": item.id,
+			"text": item.val + " ("+ item.id +")",
+			"selected": item.id == 'MXN'
+		}));
+		
+		$("#selDestino").append($("<option />", {
+			"value": item.id,
+			"text": item.val + " ("+ item.id +")",
+			"selected": item.id == 'USD'
+		}));
+    });
+    
+    $("#frmCalcular").validate({
+		debug: true,
+		rules: {
+			txtCantidad: {
+				required: true,
+				number: true
+			}
+		},
+		messages: {
+			txtCantidad: {
+				required: "Indica un valor",
+				number: "Solo números"
+			}
+		},
+		submitHandler: function(form){
+	    	if ($("#selDestino").val() == $("#selOrigen").val()){
+		    	alert("No se puede hacer la conversión");
+		    	return false;
+	    	}
+	    		
+	    	if ($("#txtCantidad").val() == '')
+	    		$("#txtCantidad").val("1");
+	    		
+	    	$("#dvResultado").html("Espere mientras obtenemos el resultado, no tardamos");
+	    	
+	    	$.get("https://www.google.com/finance/converter?hl=es&a=" + $("#txtCantidad").val() + "&from=" + $("#selOrigen").val() + "&to=" + $("#selDestino").val(), function(resp){
+	    	
+	    		resp = $($(resp).find("#currency_converter_result"));
+	    		resp.children("input").remove();
+	    		
+	    		$("#dvResultado").html(resp);
+	    	});
+	    }
+    });
+});
